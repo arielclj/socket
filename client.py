25eda1c1904cd7051373296354d007749fbae45e
arielclj <arielclj@gmail.com>
2020-10-07 16:33:43 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+
+HEADER = 1024
+PORT = 12345
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT = "bye"
+message = ""
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+while message != DISCONNECT:
+    message = input(' ---> ')
+    send(message)
+print("Bye!")
